d89545a94fe4e1e913328c70f51b6994f7136da1
sx_linzhf <1160273522@qq.com>
2023-04-03 18:27:37 +0800

    hot-fix commit2

--- main.cpp
+++ main.cpp
@@ -8,5 +8,6 @@ int main(int argc,char* argv[]) {
     printf("func_add(%d,%d) = [%d]\n",a,b,res);
     printf("func_add(%d,%d) = [%d]\n",a,b,res);
     printf("func_add(%d,%d) = [%d]\n",a,b,res);
+    printf("func_add(%d,%d) = [%d] hot-fix test\n",a,b,res);
     return 0;
 }
