a68efd21fedf3a6baa7dc3a0e173caf5bb093463
sx_linzhf <1160273522@qq.com>
2023-04-03 17:55:17 +0800

    first clion commit

---
+++ main.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include "utility.h"
+int main(int argc,char* argv[]) {
+    int a = 12;
+    int b = 23;
+    int res = func_add(a,b);
+    printf("func_add(%d,%d) = [%d]\n",a,b,res);
+    return 0;
+}
---
+++ utility.h
@@ -0,0 +1,10 @@
+//
+// Created by 123 on 2023-04-03.
+//
+
+#ifndef UNTITLED6_UTILITY_H
+#define UNTITLED6_UTILITY_H
+auto func_add(int a ,int b)->int {
+    return a + b;
+}
+#endif //UNTITLED6_UTILITY_H
