797d61b880db07fbf6a0eb46f7985e68ad5c9cde
sx_linzhf <1160273522@qq.com>
2023-04-04 15:52:39 +0800

    push github commit

--- main.cpp
+++ main.cpp
@@ -10,5 +10,6 @@ int main(int argc,char* argv[]) {
     printf("func_add(%d,%d) = [%d]\n",a,b,res);
     printf("func_add(%d,%d) = [%d]\n master test",a,b,res);
     printf("func_add(%d,%d) = [%d] hot-fix test\n",a,b,res);
+    printf("func_add(%d,%d) = [%d] push github test\n",a,b,res);
     return 0;
 }
